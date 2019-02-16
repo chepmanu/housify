f508f2d99cc4b1f098847306d7f37b388aefadd2
chepmanu <emmanuelchepsire33@gmail.com>
2019-02-16 17:46:03 +0300

    Feature(Home page): Add homepage
     - Add a home page
    
    Finishes #163550987

---
+++ src/components/Login.vue
@@ -0,0 +1,121 @@
+<template>
+  <v-app id="inspire">
+    <v-content>
+      <v-container fluid fill-height>
+        <v-layout align-center justify-center>
+          <v-flex xs12 sm8 md4>
+            <v-card class="elevation-12">
+              <v-toolbar dark color="primary">
+                <v-toolbar-title>Login form</v-toolbar-title>
+                <v-spacer></v-spacer>
+              </v-toolbar>
+              <v-card-text>
+                <v-form
+                    ref="form"
+                    v-model="valid"
+                    lazy-validation
+                >
+                    <v-text-field
+                    v-model="name"
+                    :counter="10"
+                    :rules="nameRules"
+                    label="Name"
+                    required
+                    ></v-text-field>
+
+                    <v-text-field
+                    v-model="email"
+                    :rules="emailRules"
+                    label="E-mail"
+                    required
+                    ></v-text-field>
+
+                    <v-select
+                    v-model="select"
+                    :items="items"
+                    :rules="[v => !!v || 'Item is required']"
+                    label="Item"
+                    required
+                    ></v-select>
+
+                    <v-checkbox
+                    v-model="checkbox"
+                    :rules="[v => !!v || 'You must agree to continue!']"
+                    label="Do you agree?"
+                    required
+                    ></v-checkbox>
+
+                    <v-btn
+                    :disabled="!valid"
+                    color="success"
+                    @click="validate"
+                    >
+                    Validate
+                    </v-btn>
+
+                    <v-btn
+                    color="error"
+                    @click="reset"
+                    >
+                    Reset Form
+                    </v-btn>
+
+                    <v-btn
+                    color="warning"
+                    @click="resetValidation"
+                    >
+                    Reset Validation
+                    </v-btn>
+            </v-form>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="primary">Login</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      valid: true,
+      name: '',
+      nameRules: [
+        v => !!v || 'Name is required',
+        v => (v && v.length <= 10) || 'Name must be less than 10 characters'
+      ],
+      email: '',
+      emailRules: [
+        v => !!v || 'E-mail is required',
+        v => /.+@.+/.test(v) || 'E-mail must be valid'
+      ],
+      select: null,
+      items: [
+        'Item 1',
+        'Item 2',
+        'Item 3',
+        'Item 4'
+      ],
+      checkbox: false
+    }),
+
+    methods: {
+      validate () {
+        if (this.$refs.form.validate()) {
+          this.snackbar = true
+        }
+      },
+      reset () {
+        this.$refs.form.reset()
+      },
+      resetValidation () {
+        this.$refs.form.resetValidation()
+      }
+    }
+  }
+</script>
